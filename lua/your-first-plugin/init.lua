edac8593b78fd9bf6b29b41672e32df7a6488aa7
Michael Paulson <mpaulson@netflix.com>
2020-08-29 16:30:19 -0700

    feanthoentuhnoteuhntoheuntoheutnoheutnh - Video 2

--- lua/your-first-plugin/init.lua
+++ lua/your-first-plugin/init.lua
@@ -1,11 +1,30 @@
 
-local function printWindowSize()
-    print(vim.fn.nvim_win_get_width(0),
-        vim.fn.nvim_win_get_height(0))
+local function createFloatingWindow()
+    local stats = vim.api.nvim_list_uis()[1]
+    local width = stats.width;
+    local height = stats.height;
 
-    print(vim.g["your_first_plugin_value"], vim.g["your_first_plugin_value2"])
+    print("Window size", width, height)
+
+    local bufh = vim.api.nvim_create_buf(false, true)
+    local winId = vim.api.nvim_open_win(bufh, true, {
+        relative="editor",
+        width = width - 4,
+        height = height - 4,
+        col = 2,
+        row = 2,
+    })
+end
+
+local function onResize()
+    local stats = vim.api.nvim_list_uis()[1]
+    local width = stats.width;
+    local height = stats.height;
+
+    print("Window size", width, height)
 end
 
 return {
-    printWindowSize = printWindowSize
+    createFloatingWindow = createFloatingWindow,
+    onResize = onResize
 }
--- plugin/your-first-plugin.vim
+++ plugin/your-first-plugin.vim
@@ -1,11 +1,12 @@
 fun! YourFirstPlugin()
     " dont forget to remove this one....
     lua for k in pairs(package.loaded) do if k:match("^your%-first%-plugin") then package.loaded[k] = nil end end
-    lua require("your-first-plugin").printWindowSize()
+    lua require("your-first-plugin").createFloatingWindow()
 endfun
 
 let g:your_first_plugin_value = 42
 augroup YourFirstPlugin
     autocmd!
+    autocmd VimResized * :lua require("your-first-plugin").onResize()
 augroup END
 
